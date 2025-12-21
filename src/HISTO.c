46a4913adcfc14c253c018436716fbd22f8bb2fb
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 20:16:23 +0100

    Update HISTO.c

--- src/HISTO.c
+++ src/HISTO.c
@@ -1,37 +1,34 @@
 #include "HISTO.h"
-
 #include <stdio.h>
 
-int histo_generate(const char *datafile, HistoType type, const char *outputfile) {
-    if (!datafile || !outputfile) {
+int histo_generer(const char *fichier_donnees, TypeHisto type, const char *fichier_sortie) {
+    if (!fichier_donnees || !fichier_sortie) {
         return 1;
     }
-
+    
     Usine *racine = NULL;
-
-    if (csv_parse_for_histo(datafile, &racine) != 0) {
+    if (csv_analyser_pour_histo(fichier_donnees, &racine) != 0) {
         return 1;
     }
-
-    FILE *fout = fopen(outputfile, "w");
-    if (!fout) {
-        fprintf(stderr, "Erreur: impossible d'ouvrir %s en écriture\n", outputfile);
+    
+    FILE *fsortie = fopen(fichier_sortie, "w");
+    if (!fsortie) {
+        fprintf(stderr, "Erreur: impossible d'ouvrir %s en écriture\n", fichier_sortie);
         usine_liberer(racine);
         return 2;
     }
-
+    
     if (type == HISTO_MAX) {
-        fprintf(fout, "identifier;max volume(M.m3/year)\n");
+        fprintf(fsortie, "identifier;max volume(M.m3/an)\n");
     } else if (type == HISTO_SRC) {
-        fprintf(fout, "identifier;source volume(M.m3/year)\n");
+        fprintf(fsortie, "identifier;source volume(M.m3/an)\n");
     } else {
-        fprintf(fout, "identifier;real volume(M.m3/year)\n");
+        fprintf(fsortie, "identifier;real volume(M.m3/an)\n");
     }
-
-    usine_ecrire_histo(racine, fout, (int)type);
-
-    fclose(fout);
+    
+    usine_ecrire_histo(racine, fsortie, (int)type);
+    
+    fclose(fsortie);
     usine_liberer(racine);
-
     return 0;
 }
