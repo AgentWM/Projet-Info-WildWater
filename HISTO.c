0ed4d4daa5ff2bd072bb45b48a85ca016fa7e41f
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 13:15:41 +0100

    Create HISTO.c

---
+++ HISTO.c
@@ -0,0 +1,37 @@
+#include "histo.h"
+
+#include <stdio.h>
+
+int histo_generate(const char *datafile, HistoType type, const char *outputfile) {
+    if (!datafile || !outputfile) {
+        return 1;
+    }
+
+    Usine *racine = NULL;
+
+    if (csv_parse_for_histo(datafile, &racine) != 0) {
+        return 1;
+    }
+
+    FILE *fout = fopen(outputfile, "w");
+    if (!fout) {
+        fprintf(stderr, "Erreur: impossible d'ouvrir %s en écriture\n", outputfile);
+        usine_liberer(racine);
+        return 2;
+    }
+
+    if (type == HISTO_MAX) {
+        fprintf(fout, "identifier;max volume(M.m3/year)\n");
+    } else if (type == HISTO_SRC) {
+        fprintf(fout, "identifier;source volume(M.m3/year)\n");
+    } else {
+        fprintf(fout, "identifier;real volume(M.m3/year)\n");
+    }
+
+    usine_ecrire_histo(racine, fout, (int)type);
+
+    fclose(fout);
+    usine_liberer(racine);
+
+    return 0;
+}
