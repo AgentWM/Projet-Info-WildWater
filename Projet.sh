59b61dc5858f73f42e629d764c5bb36d64961c46
Maxence Marchand <154016280+AgentWM@users.noreply.github.com>
2025-12-21 18:08:32 +0100

    Add files via upload

--- Projet.sh
+++ Projet.sh
@@ -69,21 +69,21 @@ EOF
 		#Graphique - 50 plus petites usines en capacité
 		tail -n +2 vol_max.dat | sort -t";" -k2,2n | head -n 50 > min50_max.dat
         	if command -v gnuplot &> /dev/null; then
-            	gnuplot <<EOF
-            	set datafile separator ';'
-            	set terminal pngcairo size 1280,720
-            	set output 'histo_min_50.png'
-            	set title 'Les 50 usines avec la capacité la plus faible'
-            	set ylabel 'M.m3/an'
-            	set xlabel 'Station (ID)'
-            	set style data histograms
-            	set style fill solid 1.0 border -1
-            	set boxwidth 0.8
-            	# Réglages spécifiques pour 50 car ça fait beaucoup...
-            	set xtics rotate by 90 font ",8"
-            	unset grid
-            	set grid y
-            	plot 'min50_max.dat' using 2:xtic(1) notitle
+            		gnuplot <<EOF
+            		set datafile separator ';'
+            		set terminal pngcairo size 1280,720
+            		set output 'histo_min_50.png'
+            		set title 'Les 50 usines avec la capacité la plus faible'
+            		set ylabel 'M.m3/an'
+            		set xlabel 'Station (ID)'
+            		set style data histograms
+            		set style fill solid 1.0 border -1
+            		set boxwidth 0.8
+            		# Réglages spécifiques pour 50 car ça fait beaucoup...
+            		set xtics rotate by 90 font ",8"
+            		unset grid
+            		set grid y
+            		plot 'min50_max.dat' using 2:xtic(1) notitle
 EOF
         	fi
 	   	rm min50_max.dat
