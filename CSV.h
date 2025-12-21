f389d9f8d365d147ca6f05a608b7e5240e8bca56
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 13:10:49 +0100

    Create CSV.h

---
+++ CSV.h
@@ -0,0 +1,18 @@
+#ifndef CSV_H
+#define CSV_H
+
+#include "avl_usines.h"
+#include "network.h"
+
+typedef enum {
+    HISTO_MAX = 0,
+    HISTO_SRC = 1,
+    HISTO_REAL = 2
+} HistoType;
+
+int csv_parse_for_histo(const char *filename, Usine **racine);
+
+int csv_parse_for_leaks(const char *filename, Usine **racine_usines,
+                        NodeAVL **racine_nodes);
+
+#endif
