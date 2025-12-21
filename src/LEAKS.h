e851e07c0374e55f2882c9090b5099bc46e4a842
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 20:21:02 +0100

    Update LEAKS.h

--- src/LEAKS.h
+++ src/LEAKS.h
@@ -1,10 +1,9 @@
 #ifndef LEAKS_H
 #define LEAKS_H
-
 #include "CSV.h"
 
-int leaks_compute(const char *datafile,
-                  const char *usine_id,
-                  const char *outputfile);
+int fuites_calculer(const char *fichier_donnees,
+                    const char *id_usine,
+                    const char *fichier_sortie);
 
 #endif
