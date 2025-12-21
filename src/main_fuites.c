c861046387df149a31d549f5e5ffbdd126a89dd9
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 20:29:13 +0100

    Update main_fuites.c

--- src/main_fuites.c
+++ src/main_fuites.c
@@ -3,15 +3,16 @@
 #include "LEAKS.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) { //Les 3 arguments demandés dans le shell
-        fprintf(stderr, "Usage: %s <input_file> <usine_id>\n", argv[0]);
+    if (argc != 3) { // Les 3 arguments demandés dans le shell
+        fprintf(stderr, "Usage: %s <fichier_entree> <id_usine>\n", argv[0]);
         return 1;
     }
-
-    const char *datafile = argv[1];
-    const char *usine_id = argv[2];
-    const char *outputfile = "/dev/stdout"; //Pour éviter tout problème avec >> dans le script du shell
+    
+    const char *fichier_donnees = argv[1];
+    const char *id_usine = argv[2];
+    const char *fichier_sortie = "/dev/stdout"; // Pour éviter tout problème avec >> dans le script du shell
+    
     // On appelle la fonction du groupe
-    int r = leaks_compute(datafile, usine_id, outputfile);
+    int r = fuites_calculer(fichier_donnees, id_usine, fichier_sortie);
     return r;
 }
