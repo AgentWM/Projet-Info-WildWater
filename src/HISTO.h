82e3ac514b2cefa7f86750b7894452c4a4c07bf0
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 20:14:42 +0100

    Update HISTO.h

--- src/HISTO.h
+++ src/HISTO.h
@@ -1,8 +1,7 @@
 #ifndef HISTO_H
 #define HISTO_H
-
 #include "CSV.h"
 
-int histo_generate(const char *datafile, HistoType type, const char *outputfile);
+int histo_generer(const char *fichier_donnees, TypeHisto type, const char *fichier_sortie);
 
 #endif
