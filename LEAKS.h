33b26a6143181d0b85517126b3f11ae71a4c607c
Kylian-19 <vandelkylian2006@gmail.com>
2025-12-21 13:18:45 +0100

    Create LEAKS.h

---
+++ LEAKS.h
@@ -0,0 +1,10 @@
+#ifndef LEAKS_H
+#define LEAKS_H
+
+#include "csv.h"
+
+int leaks_compute(const char *datafile,
+                  const char *usine_id,
+                  const char *outputfile);
+
+#endif
